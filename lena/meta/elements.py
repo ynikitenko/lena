ec88eeb36b9cad47ae31ac71016072d59cac57ec
Yaroslav Nikitenko <metst13@gmail.com>
2024-09-12 13:33:05 +0200

    meta.SetContext adds a method _set_context to support static context formatting (to be updated in core).

--- lena/meta/elements.py
+++ lena/meta/elements.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
 
-import lena.context
-from lena.context import update_recursively
+from lena.core import LenaAttributeError, LenaKeyError
+from lena.context import format_update_with, update_recursively
 
 
 class SetContext(object):
@@ -18,35 +18,60 @@ class SetContext(object):
     def __init__(self, key, value):
         """*key* is a string representing a
         (possibly nested) dictionary key. *value* is its value.
-        See :func:`.str_to_dict` for details.
+
+        *value* can be a formatting string.
+        See :func:`.format_context` for details.
         """
 
-        # todo: key could be a complete dictionary
+        # todo: key could be an entire dictionary
         # self._subcontext = lena.context.str_to_list(key)
         # self._value = value
         self._key = key
         self._value = value
-        context = lena.context.str_to_dict(key, value)
-        if isinstance(value, str) and '{{' in value:
-            # need to know other context to render this one
-            self._unknown_contexts = [(key, value)]
-            self._context = {}
-        else:
-            self._context = context
         self._has_no_data = True
+        try:
+            self._set_context({})
+            # may also raise formatting errors, checking early here.
+        except LenaKeyError:
+            # formatting keys missing
+            pass
 
     def _get_context(self):
-        return deepcopy(self._context)
+        try:
+            sc = self._static_context
+        except AttributeError:
+            # one does not expect el._get_context() to raise
+            # AttributeError, but this is the meaning of that exception
+            # (a class field was not initialised).
+            raise LenaAttributeError(
+                "static context missing. Run _set_context to set that."
+            )
+        # we want to keep our context safe, therefore deepcopy.
+        # It makes little difference to optimise in a Sequence.
+        return deepcopy(sc)
+
+    def _set_context(self, context):
+        # we assume that context was already deeply copied if needed
+        try:
+            format_update_with(self._key, self._value, context)
+        except LenaKeyError as exc:
+            raise exc
+        else:
+            self._static_context = context
 
     def __eq__(self, other):
         if not isinstance(other, SetContext):
             return NotImplemented
+        # we compare key and value, not the formatted value,
+        # because we are comparing elements, not their environment.
         return (self._key == other._key and self._value == other._value)
 
     def __repr__(self):
         val = self._value
         if isinstance(val, str):
             val = '"' + val + '"'
+        # representation like it was during the initialisation,
+        # not with the set context.
         return 'SetContext("{}", {})'.format(self._key, val)
 
 
@@ -54,6 +79,8 @@ class StoreContext():
     """Store static context. Use for debugging."""
 
     def __init__(self):
+        # todo: make context a read-only property.
+        # todo: add to docstring.
         self.context = {}
         # no need to print during setting context.
         # Patch this code manually if needed.
--- tests/meta/test_elements.py
+++ tests/meta/test_elements.py
@@ -2,8 +2,7 @@ from copy import deepcopy
 
 import pytest
 
-from lena.core import Sequence, Source, Split
-
+from lena.core import LenaAttributeError
 from lena.meta.elements import SetContext, UpdateContextFromStatic, StoreContext
 
 set_context_far  = SetContext("data.detector", "far")
@@ -23,155 +22,33 @@ def test_set_context():
     # _get_context works
     assert set_context_far._get_context() == {"data": {"detector": "far"}}
 
+    # not string values work
+    sc3 = SetContext("nevents", 3)
+    assert sc3._get_context() == {"nevents": 3}
 
-def test_sequence():
-    # todo: move to core.
-    data = [(0, {})]
+    ## context formatting works
+    sc4 = SetContext("full_name", "{{detector}}")
 
-    # todo: this should be changed.
-    ## static context does not alter context
-    # within a sequence
-    sfar = Sequence(set_context_far)
-    assert list(sfar.run(deepcopy(data))) == data
+    dt_context = {"detector": "far"}
+    sc4._set_context(dt_context)
+    assert sc4._get_context() == {"detector": "far", "full_name": "far"}
 
-    # static context is updated after the sequence
-    sfar_u0 = Sequence(sfar, UpdateContextFromStatic())
-    resfar_u0 = list(sfar_u0.run(deepcopy(data)))
-    assert resfar_u0[0][1] == {'data': {'detector': 'far'}}
-
-    # static context is updated after the element
-    sfar_u1 = Sequence(set_context_far, UpdateContextFromStatic())
-    resfar_u1 = list(sfar_u1.run(deepcopy(data)))
-    assert len(resfar_u1) == len(data)
-    assert resfar_u1[0][0] == data[0][0]
-    assert resfar_u1[0][1] == {'data': {'detector': 'far'}}
-
-    # outside a sequence
-    snear = Sequence(deepcopy(sfar), set_context_near)
-    assert list(snear.run(deepcopy(data))) == data
-
-    # todo: internal context overwrites external (previous) one
-    sfar_u0 = Sequence(set_context_near, deepcopy(sfar), UpdateContextFromStatic())
-    resfar_u0 = list(sfar_u0.run(deepcopy(data)))
-    assert resfar_u0[0][1] == {'data': {'detector': 'near'}}
-
-
-def test_set_context_split():
-    set_context_far = SetContext("data.detector", "far")
-    set_context_near = SetContext("data.detector", "near")
-    set_context_common = SetContext("data.lost", True)
-    call = lambda _: "we won't run this"
-    store1, store2, store3, store4 = [StoreContext() for i in range(1, 5)]
-
-    split = Split([
-        (
-            set_context_common,
-            call,
-            set_context_far,
-            store2,
-        ),
-        (
-            # common context updates the sequence containing Split
-            set_context_common,
-            call,
-            store3,
-            set_context_near,
-            # external context overwrites internal one
-            SetContext("data.cycle", 2),
-        ),
-    ])
-    assert split._get_context() == {'data': {'lost': True}}
-
-    s0 = Source(
-        SetContext("data.cycle", 1),
-        call,
-        store1,
-        split,
-        store4
-    )
-    assert store1.context == {'data': {'cycle': 1, 'lost': True}}
-    assert store2.context == {
-        'data': {'cycle': 1, 'detector': 'far', 'lost': True}
-    }
-    assert store3.context == {
-        'data': {'cycle': 1, 'detector': 'near', 'lost': True}
-    }
-    # static context is the same for the same level of nesting
-    assert store4.context == store1.context
-
-
-def test_set_template_context_split():
-    set_context_far = SetContext("data.detector", "far")
-    set_context_near = SetContext("data.detector", "near")
-    set_context_common = SetContext("data.lost", True)
-    call = lambda _: "we won't run this"
-    store1, store2, store3, store4 = [StoreContext() for i in range(1, 5)]
-
-    seq = Sequence(
-        # common context updates the sequence containing Split
-        set_context_common,
-        call,
-        store3,
-        set_context_near,
-        # external context overwrites internal one
-        SetContext("data.cycle", 2),
-        SetContext("cycle", "{{data.cycle}}"),
-        # todo: this is too complicated for now
-        # # this will be overwritten
-        # SetContext("cycle", "{{cycle}}_indeed"),
-        SetContext("detector", "maybe_{{detector}}"),
-        SetContext("detector", "{{data.detector}}"),
-    )
-    # print(seq[-1]._context)
-    assert seq._get_context()["detector"] == "maybe_near"
-
-    split = Split([
-        (
-            set_context_common,
-            call,
-            # data.cycle is set externally (and correctly)
-            SetContext("cycle", "{{data.cycle}}"),
-            # SetContext("cycle", "{{cycle}}_indeed"),
-            set_context_far,
-            store2,
-        ),
-        seq,
-    ])
-    assert split._get_context() == {'data': {'lost': True}}
-
-    s0 = Source(
-        SetContext("data.cycle", 1),
-        call,
-        store1,
-        split,
-        store4
-    )
-    assert store1.context == {
-        'data': {'cycle': 1, 'lost': True},
-        'cycle': '1',
-    }
-    assert store2.context == {
-        'data': {'cycle': 1, 'detector': 'far', 'lost': True},
-        'cycle': '1',
-    }
-    assert store3.context == {
-        'data': {'cycle': 1, 'detector': 'near', 'lost': True},
-        'detector': 'maybe_near',
-        'cycle': '1',
-        # 'cycle': '2_indeed',
-    }
-    # static context is the same for the same level of nesting
-    assert store4.context == store1.context
+    # missing context raises
+    sc5 = SetContext("full_name", "{{detector}}")
+    with pytest.raises(LenaAttributeError):
+        # detector was not set
+        sc5._get_context()
 
 
 def test_store_context():
     sc1 = StoreContext()
     sc2 = StoreContext()
     sc_empty = StoreContext()
+    context = {"a": "b"}
 
     # _set_context works
-    seq = Sequence(SetContext("a", "b"), sc1, sc2)
-
+    sc1._set_context(context)
+    sc2._set_context(context)
     assert sc1.context == {"a": "b"}
 
     # representation works
